9dd81752221d12864c4e2d796801078caf7a50b8
krisv <>
2019-07-30 16:46:21 +0800

    {/src/main/java/com/myspace/test_git_hook/test.java}

--- src/main/java/com/myspace/test_git_hook/test.java
+++ src/main/java/com/myspace/test_git_hook/test.java
@@ -12,6 +12,8 @@ public class test implements java.io.Serializable {
 
 	private java.lang.Integer test2;
 
+	private java.lang.Integer test3;
+
 	public test() {
 	}
 
@@ -31,9 +33,19 @@ public class test implements java.io.Serializable {
 		this.test2 = test2;
 	}
 
-	public test(java.lang.Integer test1, java.lang.Integer test2) {
+	public java.lang.Integer getTest3() {
+		return this.test3;
+	}
+
+	public void setTest3(java.lang.Integer test3) {
+		this.test3 = test3;
+	}
+
+	public test(java.lang.Integer test1, java.lang.Integer test2,
+			java.lang.Integer test3) {
 		this.test1 = test1;
 		this.test2 = test2;
+		this.test3 = test3;
 	}
 
 }
